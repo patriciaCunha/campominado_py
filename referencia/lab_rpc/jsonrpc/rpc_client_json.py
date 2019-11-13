990fbc7f53eacf6518e374515b6f7ce2ad7963bd
Patricia Cunha De Lima <44245633+patriciaCunha@users.noreply.github.com>
2019-11-13 08:26:33 -0300

    alteração rcp

--- referencia/lab_rpc/jsonrpc/rpc_client_json.py
+++ referencia/lab_rpc/jsonrpc/rpc_client_json.py
@@ -2,4 +2,4 @@ from jsonrpclib import Server
 
 def client():
     proxy = Server('http://localhost:7002')
-    print(proxy.printName("André", "Bessa"))
+    print(proxy.printName("Patricia", "Cunha"))
